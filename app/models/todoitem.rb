3fcde380b4f541abae777ad06ee824bd3ca75b98
Kevin Castro <cas20057@byui.edu>
2024-03-27 13:12:23 -0600

    added a model for validation

--- app/models/todoitem.rb
+++ app/models/todoitem.rb
@@ -1,2 +1,8 @@
 class Todoitem < ApplicationRecord
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :deadline, presence: true
+
+  scope :incomplete, -> { where(completed: false) } # Scope for incomplete todos
+  scope :completed, -> { where(completed: true) }
 end
